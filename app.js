ab0fc5c21a556619744d5833781aa5179136e63f
3599josephw <3599@holbertonschool.com>
2022-12-01 14:01:14 -0600

    succesful mysql query

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/filter', (req, res) => {
-  const result = filter(req, res);
-  res.send(result);
+  filter(req, res);
 });
 
 app.listen(port, () => {
@@ -33,50 +32,64 @@ function filter(req, res) {
   const { colors } = req.query;
   const { subject } = req.query;
   console.log(req.query);
-  let tmp = "SELECT episode_dates.title, month, colors, bushes FROM episode_dates\
-  JOIN colors_used ON episode_dates.title=colors_used.painting_title\
-  AND subject_matter.bushes=1\
-  AND episode_dates.month IN ('January')\
-  AND colors_used.Bright_Red=1\
-  JOIN subject_matter ON episode_dates.title=subject_matter.title;"
+  let tmp = "SELECT e.title, e.month, c.Bright_Red, p.cabin \
+  FROM episode_dates AS e  JOIN colors_used AS c ON e.title=c.painting_title\
+  JOIN subject_matter AS s ON s.title=e.title \
+  WHERE e.month IN ('January') AND c.Bright_Red = 1 AND c.Cadmium_Yellow=1  AND s.cabin = 1 AND s.diane_andre=1;"
 
 
-  let sql = "SELECT title, month"
-  if (months) {
-    let monthQuery = "AND episode_dates.month IN (";
-    for (let i = 0; i < months.length; i++) {
-      monthQuery += "'" + months[i] + "'";
-      if (i != months.length - 1) {
-        monthQuery += ", ";
+  let sql = "SELECT e.title, e.month, c.colors";
+  let subjectQuery = ""
+  if (subject) {
+    if (Array.isArray(subject)) {
+      for (let i = 0; i < subject.length; i++) {
+        sql += `, s.${subject[i]}`;
+        subjectQuery += `AND s.${subject[i]}=1`
       }
+    } else {
+      sql += `, s.${subject}`;
+      subjectQuery += `AND s.${subject}=1`
+    }
+  }
+
+  sql += " FROM episode_dates AS e JOIN colors_used AS c ON e.title=c.painting_title \
+  JOIN subject_matter AS s ON s.title=e.title";
+
+
+  let monthQuery = ""
+  if (months) {
+    monthQuery = "AND e.month IN (";
+    if (Array.isArray(months)) {
+      for (let i = 0; i < months.length; i++) {
+        monthQuery += "'" + months[i] + "'";
+        if (i != months.length - 1) {
+          monthQuery += ", ";
+        }
+      }
+    } else {
+      monthQuery += `'${months}'`
     }
     monthQuery += ")";
   }
 
+  let colorQuery = "";
   if (colors) {
     if (Array.isArray(colors)) {
       for (let i = 0; i < colors.length; i++) {
-        sql += `, ${colors[i]}`;
+        colorQuery += `AND c.${colors[i]}=1`;
       }
     } else {
-      sql += `, ${colors}`;
+      colorQuery += `AND c.${colors}=1`;
     }
   }
-
-  if (subject) {
-    if (Array.isArray(subject)) {
-      for (let i = 0; i < subject.length; i++) {
-        sql += `, ${subject[i]}`;
-      }
-    } else {
-      sql += `, ${subject}`;
-    }
+  if (subject || months || colors) {
+    sql += " WHERE e.title=s.title ";
   }
+  sql += `${monthQuery} ${colorQuery} ${subjectQuery}`;
 
-
-  con.query(tmp, function (err, result, fields) {
+  con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
+    res.send(result);
   });
   console.log(sql);
 }
